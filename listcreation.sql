6bef9703916a1b940feebe1d1b9a0e69e6d1d057
Megala J <megalajeeva1999@gmail.com>
2019-12-26 14:17:58 +0530

    Update listcreation.sql

--- listcreation.sql
+++ listcreation.sql
@@ -1,17 +1,17 @@
-create table list(
+drop table tlist;
+create table tlist(
 task varchar(60) not null,
-name varchar(50) not null,
+person_name varchar(50) not null,
 created_date date not null,
 deadline_date date,
 modified_date date default SYSDATE,
 completed_on date,
-priority number,
 status varchar(20)not null,
-constraint priority_pk primary key(priority)
+priority_pk number primary key
 );
-insert into list(task,name,created_date,deadline_date,completed_on,priority,status)
-values('oracle','mega',to_date('12-12-2019','dd-MM-yyyy'),to_date('25-12-2019','dd-MM-yyyy'),to_date('25-12-2019','dd-MM-yyyy'),2,'completed');
+insert into tlist(task,person_name,created_date,deadline_date,completed_on,priority_pk,status)
+values('oracle','mega',TO_DATE('12-12-2019','dd-MM-yyyy'),TO_DATE('25-12-2019','dd-MM-yyyy'),TO_DATE('25-12-2019','dd-MM-yyyy'),2,'completed');
 
-insert into list(task,name,created_date,deadline_date,completed_on,priority,status)
-values('jdk','anu',to_date('11-12-2019','dd-MM-yyyy'),to_date('15-12-2019','dd-MM-yyyy'),to_date('13-12-2019','dd-MM-yyyy'),1,'completed');
-select * from list order by priority ASC;
+insert into tlist(task,person_name,created_date,deadline_date,completed_on,priority_pk,status)
+values('jdk','anu',TO_DATE('11-12-2019','dd-MM-yyyy'),TO_DATE('15-12-2019','dd-MM-yyyy'),TO_DATE('13-12-2019','dd-MM-yyyy'),1,'completed');
+select *from tlist order by priority_pk ASC;
