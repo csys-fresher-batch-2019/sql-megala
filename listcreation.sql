5a70392c4c4eb9b94eb49608ed4be0770c6313bb
Megala J <megalajeeva1999@gmail.com>
2019-12-26 12:07:10 +0530

    Update listcreation.sql

--- listcreation.sql
+++ listcreation.sql
@@ -1,10 +1,17 @@
 create table list(
 task varchar(60) not null,
-by varchar(50) not null,
+name varchar(50) not null,
+created_date date not null,
 dealine date,
+modified_date date default SYSDATE,
 completed_on date,
-status varchar(20)not null
+priority number,
+status varchar(20)not null,
+constraint priority_pk primary key(priority)
 );
-insert into list(task,by,deadline,completed_on,status)
-values('oracle','mega',to_date('25-12-2019','dd-MM-yyyy'),to_date('25-12-2019','dd-MM-yyyy'),'completed');
-select * from list where status='completed';
+insert into list(task,name,created_date,deadline,completed_on,priority,status)
+values('oracle','mega',to_date('12-12-2019','dd-MM-yyyy'),to_date('25-12-2019','dd-MM-yyyy'),to_date('25-12-2019','dd-MM-yyyy'),2,'completed');
+
+insert into list(task,name,created_date,deadline,completed_on,priority,status)
+values('jdk','anu',to_date('11-12-2019','dd-MM-yyyy'),to_date('15-12-2019','dd-MM-yyyy'),to_date('13-12-2019','dd-MM-yyyy'),1,'completed');
+select * from list order by ASC;
