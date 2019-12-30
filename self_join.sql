b63302b2a6850055f47d80cdf8ac187efc7d2a12
Megala J <megalajeeva1999@gmail.com>
2019-12-30 12:37:14 +0530

    Update self_join.sql

--- self_join.sql
+++ self_join.sql
@@ -2,18 +2,18 @@ drop table emp;
 create table emp(
 emp_id number not null,
 emp_name varchar2(50),
-mrg_id number
+manager number
 );
 
 insert into emp(emp_id,emp_name)
 values(1,'karthi');
 
-insert into emp(emp_id,emp_name,mrg_id)
-values(2,'sandhi',8);
+insert into emp(emp_id,emp_name,manager)
+values(2,'sandhi',1);
 
-insert into emp(emp_id,emp_name,mrg_id)
-values(3,'bharu',9);
+insert into emp(emp_id,emp_name,manager)
+values(3,'bharu',2);
 
 select * from emp;
 
-select e1.emp_id,e1.emp_name,e2.emp_name as mrg_id from emp e1,emp e2 where e1.mrg_id=e2.emp_id;
+select e1.emp_id,e1.emp_name,e2.emp_name as manager from emp e1,emp e2 where e1.manager=e2.emp_id;
